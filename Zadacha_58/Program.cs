38410404ff83b2cce500512200bd83934da836e8
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-27 20:45:39 +0500

    Updated Zadacha_58 and Updated MyClassLibrary added folder HighMathMy

--- Zadacha_58/Program.cs
+++ Zadacha_58/Program.cs
@@ -11,3 +11,26 @@
 // 
 
 using MyClassLibrary;
+
+int matrixArr1X = 2, matrixArr1Y = 2;
+double[,] matrixArr1 = new double[matrixArr1X,matrixArr1Y];
+
+int matrixArr2X = 2, matrixArr2Y = 2;
+double[,] matrixArr2 = new double[matrixArr2X,matrixArr2Y];
+
+double[,] matrixArrResult = new double[matrixArr2X, matrixArr2Y];
+
+matrixArr1 = ArrayMy.Fill2DArrayToNumbers(inputArray2D: matrixArr1, fromMinNumber:-5, toMaxNumber:10);
+matrixArr2 = ArrayMy.Fill2DArrayToNumbers(inputArray2D: matrixArr2, fromMinNumber:-5, toMaxNumber:10);
+
+matrixArrResult = HighMathMy.MatrixMultiplitation(matrixArr1,matrixArr2);
+
+
+Console.WriteLine("Эта матрица 1.");
+ArrayMy.WriteArray2D(matrixArr1);
+
+Console.WriteLine("Эта матрица 2.");
+ArrayMy.WriteArray2D(matrixArr2);
+
+Console.WriteLine("Результат произедений, двух матриц, 1 и 2.");
+ArrayMy.WriteArray2D(matrixArrResult);
