10664b7dceefb67175a612326b9dfb7d2fae82f2
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-29 12:59:16 +0500

    Zadacha_54 нужен откат

--- Zadacha_54/Program.cs
+++ Zadacha_54/Program.cs
@@ -12,8 +12,11 @@
 // 9 5 3 2
 // 8 4 4 2
 
+using System;
+using System.Linq;
+
 using MyClassLibrary;
-using NoMyClassLibrary;
+using NoMyLibrary;
 
 
 
@@ -26,13 +29,38 @@ ArrayMy.WriteArray2D(arrayNumbers);
 
 Console.WriteLine("\n");
 
-for (int i = 0; i < arrayNumbers.GetLength(0); i++)
+double[] arrayNumbersIndexRow = ArrayMy.array2DGetRow1D(arrayNumbers, 0);
+
+ArrayMy.WriteArray(arrayNumbersIndexRow);
+
+
+while (true)
 {
-    
+    bool isSort = true;
+    ArrayMy.WriteArray(arrayNumbersIndexRow, " ");
+    for (int indexCollum = 1; indexCollum < arrayNumbersIndexRow.GetLength(0); indexCollum++)
+    {
+        double firstValue = arrayNumbersIndexRow[indexCollum - 1];  // 2
+        double secondValue = arrayNumbersIndexRow[indexCollum];        // 1
 
-        SortNoMy.qSort(arrayNumbers.GetLength(1), 0, arrayNumbers.GetLength(1) - 1);
+        if (secondValue <= firstValue)
+        {
+            double tempValue = secondValue;
+            secondValue = firstValue;
+            firstValue = tempValue;
+        }
 
-    
+    }
+    Console.WriteLine();
+
+    // for (int indexCollum = 1; indexCollum < arrayNumbersIndexRow.GetLength(0); indexCollum++)
+    // {
+    //     double selectionValue = arrayNumbersIndexRow[indexCollum] - 1;  // 2
+    //     double compareValue = arrayNumbersIndexRow[indexCollum];        // 1
+    //     if (compareValue >= selectionValue) {}
+    //     else isSort = false;
+    // }
+    // if (isSort == true) break;
+
 }
 
-ArrayMy.WriteArray2D(arrayNumbers);
