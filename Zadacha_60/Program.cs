f1dc07e9412cbb1445486c963b0d8c583c07729c
Roman Magafurov <roman.mag1997@yandex.ru>
2022-10-01 01:40:29 +0500

    Updated Zadacha_60 Reviuer OK

--- Zadacha_60/Program.cs
+++ Zadacha_60/Program.cs
@@ -13,19 +13,100 @@
 
 using MyClassLibrary;
 
-int lenX = 2, lenY = 2, lenZ = 2;
-double minNum = 10, maxNum = 100;
-
-int[,,] arrayNumbers = ArrayMy.Array3dIntegerRandomNoRepeat(lenX, lenY, lenZ, minNum, maxNum);
-
-for (int i = 0; i < lenX; i++)
+string IsNumber20(int number)
 {
-    for (int j = 0; j < lenY; j++)
+    switch (number % 20)
     {
-        for (int k = 0; k < lenZ; k++)
-        {
-            Console.Write($"{arrayNumbers[i, j, k]}({i},{j},{k}) ");
-        }
-        Console.WriteLine("\n");
+        case 1:
+            return "ый";
+        case 2:
+            return "ой";
+        case 3:
+            return "ий";
+        case 4:
+            return "ый";
+        case 5:
+            return "ый";
+        case 6:
+            return "ой";
+        case 7:
+            return "ой";
+        case 8:
+            return "ой";
+        case 9:
+            return "ый";
+        case 10:
+            return "ый";
+        case 11:
+            return "ый";
+        case 12:
+            return "ый";
+        case 13:
+            return "ый";
+        case 14:
+            return "ый";
+        case 15:
+            return "ый";
+        case 16:
+            return "ый";
+        case 17:
+            return "ый";
+        case 18:
+            return "ый";
+        case 19:
+            return "ый";
+        case 20:
+            return "ый";
+        default:
+            return "ой";
     }
 }
+
+
+
+int lenX = 8, lenY = 8, lenZ = 8;
+double minNum = 0, maxNum = 1000;
+
+int[,,] arrayNumbers = ArrayMy.Array3dIntegerRandomNoRepeat(lenZ, lenY, lenX, minNum, maxNum);
+
+for (int i = 0; i < lenZ; i++)
+{
+    Console.WriteLine("\n\n");
+    Console.WriteLine($"\n{i+1}-{IsNumber20(i+1)} слой.");
+    for (int j = 0; j < lenY; j++)
+    {
+        for (int k = 0; k < lenX; k++)
+        {
+            string value = Convert.ToString(arrayNumbers[i, j, k]);
+            for (int v = value.Length; v < Convert.ToString(maxNum).Length; v++)
+            {
+                value = "0" + value;
+            }
+
+            string valueI = Convert.ToString(i);
+            for (int vi = valueI.Length; vi < Convert.ToString(lenZ).Length; vi++)
+            {
+                valueI = "0" + valueI;
+            }
+
+            string valueJ = Convert.ToString(j);
+            for (int vj = valueJ.Length; vj < Convert.ToString(lenY).Length; vj++)
+            {
+                valueJ = "0" + valueJ;
+            }
+
+            string valueK = Convert.ToString(k);
+            for (int vk = valueK.Length; vk < Convert.ToString(lenX).Length; vk++)
+            {
+                valueK = "0" + valueK;
+            }
+
+
+            Console.Write($"{value}({valueK},{valueJ},{valueI})    ");
+        }
+        Console.WriteLine("\n\n\n");
+    }
+    
+}
+
+            
